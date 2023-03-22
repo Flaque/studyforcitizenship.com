a773cbd39e0e996cfee656d84991122c617297aa
Evan Conrad <evan@roomservice.dev>
2023-03-22 11:21:14 -0700

    fix

--- app/[lang]/study/page.tsx
+++ app/[lang]/study/page.tsx
@@ -8,66 +8,57 @@ import dynamic from "next/dynamic";
 const useQuestions = (language: string) => {
   const [questions, setQuestions] = useState([]);
   useEffect(() => {
-    import(`../../../data/${language}/questions.json`).then((data) => {
-      setQuestions(data.questions);
-    });
+    import(`../../../data/${language}/questions.json`)
+      .then((data) => {
+        setQuestions(data.default);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
   }, [language]);
   return questions;
 };
 
-// Uses local storage
-function useLocalStorage(key: string, defaultValue: any) {
-  const [state, setState] = useState(() => {
-    const localData =
-      typeof window !== "undefined" && window.localStorage.getItem(key);
-    return localData ? JSON.parse(localData) : defaultValue;
-  });
-
-  useEffect(() => {
-    typeof window !== "undefined" &&
-      window.localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
-
-  return [state, setState];
+function Avatar(props: { question: string; highlighted: boolean }) {
+  return (
+    <div className="flex-1 items-center justify-center flex">
+      <img
+        src={`/api/svgs/${encodeURIComponent(props.question || "foo")}`}
+        className={cn({
+          "w-4 flex mx-auto rounded-full border border-black": true,
+          "w-6": props.highlighted,
+          "opacity-50": !props.highlighted,
+        })}
+      />
+    </div>
+  );
 }
 
 export default function StudyPage() {
-  const language = "en";
+  const language = "es";
   const questions = useQuestions(language);
 
   // Two queues.
-  let [current, setCurrent] = useLocalStorage(language + ":current", []);
-  const [past, setPast] = useLocalStorage(language + ":past", []);
+  let [current, setCurrent] = useState<Array<any>>([]);
 
   // current can only have 10 questions
   if (current.length > 10) {
     current = current.slice(0, 10);
   }
 
-  // If current is empty, fill it with 10 questions
   useEffect(() => {
-    if (!questions) return;
-
+    // If there are nothing in current, then we need to fetch some.
+    if (questions.length === 0) return;
     if (current.length === 0) {
-      const newCurrent = questions.slice(0, 10);
-      setCurrent(newCurrent);
+      setCurrent(questions.slice(0, 10));
     }
-  }, [current, questions]);
+  }, [questions]);
 
   const first = current[0];
 
   const avatars = current.map((q: any, i: number) => {
     return (
-      <div key={q} className="flex-1 items-center justify-center flex">
-        <img
-          src={`/api/svgs/${encodeURIComponent(q?.question || "foo")}`}
-          className={cn({
-            "w-4 flex mx-auto rounded-full border border-black": true,
-            "w-6": i === 0,
-            "opacity-50": i > 0,
-          })}
-        />
-      </div>
+      <Avatar key={q.question} question={q.question} highlighted={i === 0} />
     );
   });
 
@@ -89,13 +80,19 @@ export default function StudyPage() {
   }
 
   if (!first) return <div></div>;
+  if (!questions) return <div></div>;
 
   return (
     <div className="px-4 py-4 bg-gray-50 h-screen">
       <div className="flex flex-col gap-4 pt-4 max-w-xl mx-auto">
         <div className="flex flex-1 justify-start w-64">{avatars}</div>
 
-        <h1 className="text-xl font-bold">{first?.question}</h1>
+        <h1
+          className="text-xl font
+        -bold"
+        >
+          {first?.question}
+        </h1>
         <textarea
           className="w-full h-32 border rounded p-2 border-gray-400"
           placeholder="Type your answer here..."
