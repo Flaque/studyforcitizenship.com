dd9b1bad453306d89b9a473b1f917f2da495ee26
Evan Conrad <evan@roomservice.dev>
2023-03-24 09:58:03 -0700

    add layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import "./globals.css";
 
 export const metadata = {
@@ -13,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
